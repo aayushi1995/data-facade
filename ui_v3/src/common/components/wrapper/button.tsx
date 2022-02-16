d13959791aadb22ce73362e6951951b68a302472
Darshan Rathi <darshan.rathi@datafacade.onmicrosoft.com>
2022-02-16 07:14:45 +0000

    Merged PR 601: Button Wrapper
    button wrapper

---
+++ ui_v3/src/common/components/wrapper/button.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@material-ui/core"
+
+
+export const DataFacadeButton = (props ?: any) => {
+    return (
+        <Button {...props}/>
+    )
+}
+
+export default DataFacadeButton
