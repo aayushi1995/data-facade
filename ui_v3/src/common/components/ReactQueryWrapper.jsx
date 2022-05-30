cc083c3b8e620b1512688391835e03795f8db45b
Shishir Arora <shishir.arora@datafacade.onmicrosoft.com>
2022-05-30 14:05:46 +0000

    Merged PR 815: react-query version increased as it has better rendering performance, also, a...
    react-query version increased as it has better rendering performance, also, adding withReactQueryWrapper HOC pattern

--- ui_v3/src/common/components/ReactQueryWrapper.jsx
+++ ui_v3/src/common/components/ReactQueryWrapper.jsx
@@ -20,5 +20,17 @@ const _ReactQueryWrapper = ({
     }
     return El;
 }
-
+export const withReactQueryWrapper = (Component, {isLoading, error, data, sx= {}}) => {
+    return ()=>{
+        let El;
+        if (isLoading) {
+            El =  <Box sx={sx}><LoadingIndicator /></Box>
+        } else if (error || !isLoading && (!data || data?.length <= 0 )) {
+            El = (<Box sx={sx}><NoData /></Box>)
+        } else {
+            El = <Component data={data}/>;
+        }
+        return El;
+    }
+}
 export const ReactQueryWrapper = (props) => <ErrorBoundry><_ReactQueryWrapper {...props} /></ErrorBoundry>
