3db8fc41b32455ec667800ed5621f0d9518a2ef0
Darshan Rathi <darshan.rathi@datafacade.onmicrosoft.com>
2022-05-31 06:04:07 +0000

    Merged PR 819: Revert 'react-query version increased as it has better rendering performance, also, a...'
    react-query version increased as it has better rendering performance, also, adding withReactQueryWrapper HOC pattern
    
    Reverts !815

--- ui_v3/src/common/components/application/rows/PreBuiltApplications.tsx
+++ ui_v3/src/common/components/application/rows/PreBuiltApplications.tsx
@@ -1,31 +1,33 @@
-import {useGetPrebuiltApplications} from "./../hooks/useGetPrebuildApplications"
-import {Box, Grid} from "@mui/material"
+import { useGetPrebuiltApplications } from "./../hooks/useGetPrebuildApplications"
+import { Box, Card, Grid } from "@mui/material"
 import ApplicationCard from "./../ApplicationCard"
-import {ApplicationCardViewResponse} from "./../../../../generated/interfaces/Interfaces"
-import {AllApplicationRowProps} from "../../../../pages/apps/components/AllApplicationView"
-import {withReactQueryWrapper} from "../../ReactQueryWrapper";
+import { ApplicationCardViewResponse } from "./../../../../generated/interfaces/Interfaces"
+import LoadingWrapper from "../../LoadingWrapper"
+import { AllApplicationRowProps } from "../../../../pages/apps/components/AllApplicationView"
 
 
 const PreBuiltApplications = (props: AllApplicationRowProps) => {
-    const searchQuery = props.searchQuery || ""
+    const searchQuery = props.searchQuery||""
     const prebuiltAppsQuery = useGetPrebuiltApplications()
 
-    const PrebuiltApplicationCards = ({data: prebuiltApplications}: { data: ApplicationCardViewResponse[] | undefined }) => {
-        return <Grid container spacing={1} sx={{overflowX: 'auto', minWidth: '100%'}}>{
-            (prebuiltApplications || [])
-                .filter(prebuiltApplication => prebuiltApplication.ApplicationName?.toLowerCase().includes(searchQuery.toLowerCase()) || prebuiltApplication.ApplicationName?.toLowerCase().includes(searchQuery.toLowerCase()))
-                .map(prebuiltApplication =>
-                    <Grid item xs={12} lg={3} sm={6} md={4}>
-                        <Box sx={{height: '100%', p: 2}} key={prebuiltApplication.ApplicationId}>
-                            <ApplicationCard application={prebuiltApplication}/>
-                        </Box>
-                    </Grid>
-                )}
-        </Grid>;
+    const renderCards = (prebuiltApplications: ApplicationCardViewResponse[] | undefined) => {
+        const filteredApplications = (prebuiltApplications||[]).filter(prebuiltApplication => prebuiltApplication.ApplicationName?.toLowerCase().includes(searchQuery.toLowerCase())||prebuiltApplication.ApplicationName?.toLowerCase().includes(searchQuery.toLowerCase()))
+
+        return filteredApplications.map(prebuiltApplication => 
+            <Grid item xs={12} lg={3} sm={6} md={4}>
+                <Box sx={{ height: '100%', p: 2}}  key={prebuiltApplication.ApplicationId}>
+                    <ApplicationCard application={prebuiltApplication}/>
+                </Box>
+            </Grid>
+        )
     }
-    const PrebuiltApplicationCardsWithData = withReactQueryWrapper(PrebuiltApplicationCards, prebuiltAppsQuery);
-    return (
-        <PrebuiltApplicationCardsWithData/>
+
+    return(
+        <LoadingWrapper isLoading={prebuiltAppsQuery.isLoading} error={prebuiltAppsQuery.error} data={prebuiltAppsQuery.data}>
+            <Grid container spacing={1} sx={{overflowX: 'auto', minWidth: '100%'}}>
+                {renderCards(prebuiltAppsQuery.data)}
+            </Grid>
+        </LoadingWrapper>
     )
 }
 
