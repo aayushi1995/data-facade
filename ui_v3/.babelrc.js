5fc3da708417b9071d74cf767601ecbfa1988278
Kallol Pratim Dev <kallolpratim.dev@datafacade.onmicrosoft.com>
2023-02-06 05:45:33 +0000

    Merged PR 1321: config override to compress file
    config override to compress file

--- ui_v3/.babelrc.js
+++ ui_v3/.babelrc.js
@@ -1,36 +1,36 @@
 const plugins = [
-  [
-    "babel-plugin-import",
-    {
-      libraryName: "@mui/material",
-      libraryDirectory: "",
-      camel2DashComponentName: false,
-    },
-    "core",
-  ],
-  [
-    "babel-plugin-direct-import",
-    { modules: ["@mui/material", "@mui/icons-material"] },
-  ],
-  [
-    "babel-plugin-import",
-    {
-      libraryName: "@mui/icons-material",
-      libraryDirectory: "",
-      camel2DashComponentName: false,
-    },
-    "icons",
-  ],
-  ["import", {
-    "libraryName": "echarts",
-    "libraryDirectory": "src/components"
-  }],
-  ["import", {
-    "libraryName": "xlsx",
-    "libraryDirectory": "",
-    "camel2DashComponentName": false
-  }],
-  "equire"
-];
-
-module.exports = { plugins };
+    [
+   "babel-plugin-import",
+      {
+   libraryName: "@mui/material",
+   libraryDirectory: "",
+   camel2DashComponentName: false,
+      },
+   "core",
+    ],
+    [
+   "babel-plugin-direct-import",
+      { modules: ["@mui/material", "@mui/icons-material"] },
+    ],
+    [
+   "babel-plugin-import",
+      {
+   libraryName: "@mui/icons-material",
+   libraryDirectory: "",
+   camel2DashComponentName: false,
+      },
+   "icons",
+    ],
+    ["import", {
+   "libraryName": "echarts",
+   "libraryDirectory": "src/components"
+    }],
+    ["import", {
+   "libraryName": "xlsx",
+   "libraryDirectory": "",
+   "camel2DashComponentName": false
+    }],
+   "equire"
+  ];
+  
+  module.exports = { plugins };
--- ui_v3/config-overrides.js
+++ ui_v3/config-overrides.js
@@ -1,11 +1,42 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = function override(webpackConfig) {
-    webpackConfig.module.rules.push({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: "javascript/auto"
-    });
-    webpackConfig.plugins = process.env.NODE_ENV === 'production' ? webpackConfig.plugins.filter(
-        (plugin) => !(plugin.options && plugin.options.eslintPath)
-    ) : webpackConfig.plugins;
-    return webpackConfig;
-}
+  webpackConfig.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: "javascript/auto",
+  });
+  webpackConfig.plugins =
+    process.env.NODE_ENV === "production"
+      ? webpackConfig.plugins.filter(
+          (plugin) => !(plugin.options && plugin.options.eslintPath)
+        )
+      : webpackConfig.plugins;
+
+  if (process.env.NODE_ENV === "production") {
+    webpackConfig.optimization = {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true,
+              drop_debugger: true
+            },
+            output: {
+              comments: false
+            }
+          },
+        }),
+      ],
+    };
+  }
+
+  webpackConfig.optimization.splitChunks = {
+    chunks: "all",
+  };
+
+  
+
+  return webpackConfig;
+};
