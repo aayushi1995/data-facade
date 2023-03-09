f80eb20548f7fd8b4a76bc1a4c379cf171d25968
Ritesh Singh <ritesh286singh@gmail.com>
2023-03-09 09:04:29 +0000

    Merged PR 1467: Hubspot Mixpanel
    changes

--- ui_v3/src/pages/data/components/connections/iconProviderMap.ts
+++ ui_v3/src/pages/data/components/connections/iconProviderMap.ts
@@ -1,14 +1,15 @@
 import S3 from './images/Amazon_Web_Services-Logo.wine.svg';
+import Google from './images/Google.svg';
+import Hubspot from './images/Hubspot.png';
+import Mixpanel from './images/Mixpanel.png';
+import PostgreSql from './images/PostgreSQL-Logo.wine.svg';
+import Salesforce from './images/Salesforce.com-Logo.wine.svg';
 import Clickhouse from './images/clickhouse.svg';
 import Databricks from './images/databricks.svg';
 import DBTRepo from './images/gitdbt.svg';
-import Google from './images/Google.svg';
-import PostgreSql from './images/PostgreSQL-Logo.wine.svg';
 import Redshift from './images/redshift.svg';
-import Salesforce from './images/Salesforce.com-Logo.wine.svg';
 import Slack from './images/slack.svg';
 import Snowflake from './images/snowflake.svg';
-
 
 export const iconProviderMap = {
     Databricks,
@@ -20,5 +21,7 @@ export const iconProviderMap = {
     DBTRepo,
     Clickhouse,
     Slack,
-    Google
+    Google,
+    Hubspot,
+    Mixpanel
 }
